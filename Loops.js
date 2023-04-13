0fed1cf0638b56a9b969d9e380e05e119394bb39
Jose Reyna <jose-atil@hotmail.com>
2023-04-13 13:54:18 -0700

    Clases terminadas de LOOPS

--- Loops.js
+++ Loops.js
@@ -18,3 +18,28 @@ function saludarEstudiantes(estudiante) {
 for(var estudiante of estudiantes){
     saludarEstudiantes(estudiante);
 }
+
+// Metodo while
+    var estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"];
+
+    function saludarEstudiantes(estudiante){
+        console.log(`hola, ${estudiante}`);
+    }
+
+    while(estudiantes.length > 0){
+        var estudiante = estudiantes.shift();
+        saludarEstudiantes(estudiante);
+    }
+
+    // Metodo 2 while
+    var estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"];
+
+    function saludarEstudiantes(estudiante){
+        console.log(`hola, ${estudiante}`);
+    }
+
+    while(estudiantes.length > 0){
+        console.log(estudiante)
+        var estudiante = estudiantes.shift();
+        saludarEstudiantes(estudiante);
+    }
