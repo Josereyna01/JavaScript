9a400dc77131a9f91ef2de11a2c580a2018e7fa6
Jose Reyna <jose-atil@hotmail.com>
2023-04-14 13:55:05 -0700

    Reccordiendo arrays con find, forEach y Some

--- RecorridosDeArrays.js
+++ RecorridosDeArrays.js
@@ -15,3 +15,30 @@ var articulosFiltrados = articulos.filter(function (articulo) {
 var nombreArticulos = articulos.map(function(articulo){
     return articulo.nombre
 });
+
+// Segunda Clase
+
+var articulos = [
+    {nombre: "Bici", costo: 3000 },
+    {nombre: "Tv", costo: 2500},
+    {nombre: "Libro", costo: 320},
+    {nombre: "Celular", costo: 10000},
+    {nombre: "Laptop", costo: 20000},
+    {nombre: "Teclado", costo: 500},
+    {nombre: "Audifonos", costo: 1700}
+];
+
+// Find
+var encuentraArticulo = articulos.find(function(articulo){
+    return articulo.nombre === "Laptop"
+});
+
+// forEach
+articulos.forEach(function(articulo){
+    console.log(articulo.nombre);
+})
+
+// Some
+var articulosBaratos = articulos.some(function(articulo){
+    return articulo.costo <= 700;
+});
